e25a10a9e37ef1094ff26b26448196d6a4d6643a
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-08-10 21:19:35 +1000

    Building stream functionality

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,18 +3,24 @@
 import React, {useState} from 'react';
 import HomeCard from "@/components/HomeCard";
 import {useRouter} from "next/navigation";
+import MeetingModal from "@/components/MeetingModal";
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' |
-    'isJoiningMeeting' | undefined>()
+    'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+
+    const createMeeting = () => {
+
+    }
+
     return (
         <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
                 <HomeCard
                     img='/icons/add-meeting.svg'
                     title="New Meeting"
                     description='Start a instant meeting'
-                    handleClick={() => setMeetingState('isJoiningMeeting')}
+                    handleClick={() => setMeetingState('isInstantMeeting')}
                     className='bg-orange-1'
                 />
                 <HomeCard
@@ -37,6 +43,15 @@ const MeetingTypeList = () => {
                     description='via invitation link'
                     handleClick={() => setMeetingState('isJoiningMeeting')}
                     className='bg-yellow-1'
+                />
+
+                <MeetingModal
+                    isOpen={meetingState === 'isInstantMeeting'}
+                    onClose={() => setMeetingState(undefined)}
+                    title="Start an Instant Meeting"
+                    className='text-center'
+                    buttonText='Start Meeting'
+                    handleClick={createMeeting}
                 />
         </section>
     );
